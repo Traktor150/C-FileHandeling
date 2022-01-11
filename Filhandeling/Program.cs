2de77843dc0e0f249dfc377fbff6238b38bb721b
William <williambecker321@gmail.com>
2022-01-11 13:33:51 +0100

    Now appends text to file insted of writing over.

--- Filhandeling/Program.cs
+++ Filhandeling/Program.cs
@@ -13,10 +13,12 @@ namespace Filhandeling
             Console.WriteLine("Ange ett namn.");
             namn = Console.ReadLine();
 
-            using (StreamWriter sw = File.CreateText(path))
-            {
-                sw.WriteLine(namn);
-            }
+            // using (StreamWriter sw = File.CreateText(path))
+            // {
+            //     sw.WriteLine(namn);
+            // }
+
+            File.AppendAllText(path, namn);
 
             using (StreamReader sr = File.OpenText(path))
             {
